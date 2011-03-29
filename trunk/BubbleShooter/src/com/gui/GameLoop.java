250b1a6600dcd88e4e1125597a92d9b63179eaa9
davidsen86@gmail.com <davidsen86@gmail.com@7185608a-03ba-cb5d-678c-909f53f12cc8>
2011-03-29 19:27:34 +0000

    Tilføjet GameLoop.java

---
+++ trunk/BubbleShooter/src/com/gui/GameLoop.java
@@ -0,0 +1,42 @@
+package com.gui;
+
+import com.badlogic.gdx.Application;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL10;
+import com.model.Renderer;
+import com.simulation.Simulation;
+
+public class GameLoop implements Frame {
+	private final Simulation simulation;
+	private final Renderer renderer;
+	private boolean isDisposable = false;
+
+
+	public GameLoop(Application app){
+		simulation = new Simulation();
+		renderer = new Renderer(app);
+	}
+	
+	@Override
+	public void update(Application app) {
+		simulation.update(app.getGraphics().getDeltaTime());
+	}
+
+	@Override
+	public void render(Application app) {
+		app.getGraphics().getGL10().glClear(GL10.GL_COLOR_BUFFER_BIT);
+		renderer.render(app, simulation);
+		Renderer.draw("FPS: "+Gdx.graphics.getFramesPerSecond(), 150, 150);
+	}
+
+	@Override
+	public boolean isDisposable() {
+		return isDisposable;
+	}
+
+	@Override
+	public void dispose() {
+		renderer.dispose();
+	}
+
+}
