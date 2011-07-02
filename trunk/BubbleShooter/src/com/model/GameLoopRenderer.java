7389b105e7911d99f25b46b8db37b3a52420edd6
davidsen86@gmail.com <davidsen86@gmail.com@7185608a-03ba-cb5d-678c-909f53f12cc8>
2011-07-02 22:21:37 +0000

    deleted button.java added shooter to gameloop

--- trunk/BubbleShooter/src/com/model/GameLoopRenderer.java
+++ trunk/BubbleShooter/src/com/model/GameLoopRenderer.java
@@ -11,6 +11,8 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Matrix4;
+import com.gui.GameLoop;
+import com.simulation.AnimatedSprite;
 import com.simulation.Simulation;
 
 public class GameLoopRenderer {
@@ -19,16 +21,22 @@ public class GameLoopRenderer {
 	private final Matrix4 transformMatrix = new Matrix4();
 	private static SpriteBatch spriteBatch;
 	private static BitmapFont font;
-	
+	public AnimatedSprite shooter;
+	private Texture shooterAnimation;
+	private GameLoop gameloop;
+
 	/**
 	 * Bruges til at adskille renderer metoderne fra GameLoop
+	 * 
 	 * @param app
 	 */
-	public GameLoopRenderer(Application app) {
+	public GameLoopRenderer(GameLoop gameloop) {
+		this.gameloop = gameloop;
 		background = new TextureAtlas();
 		for (int i = 1; i <= 8; i++) {
 			Texture backgroundPart = new Texture(
-					Gdx.files.internal("res/bg/background_portrait_" + i + ".png"));
+					Gdx.files.internal("res/bg/background_portrait_" + i
+							+ ".png"));
 			backgroundPart
 					.setFilter(TextureFilter.Linear, TextureFilter.Linear);
 			TextureRegion r = new TextureRegion();
@@ -38,16 +46,28 @@ public class GameLoopRenderer {
 		}
 		font = new BitmapFont();
 		font.setScale(2, 2);
+		populate();
 	}
 
 	public void render(Application app, Simulation simulation) {
-		GL10 gl = app.getGraphics().getGL10();
-		gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
-		
-		renderBackground(gl);
+		// GL10 gl = app.getGraphics().getGL10(); // Skal kaldes hvis GL skal
+		// bruges igen
+		// gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+		renderBackground();
+		renderShooter(shooter);
 	}
-	
-	private void renderBackground(GL10 gl) {
+
+	public void populate() {
+		shooterAnimation = new Texture(Gdx.files.internal("res/shooter.png"),
+				true);
+		shooterAnimation.setFilter(TextureFilter.MipMap, TextureFilter.Linear);
+		shooter = new AnimatedSprite("shooter", shooterAnimation, 0, 0, 64,
+				64, 1, 1, 0, 0);
+		shooter.setPosition((480 - 64) / 2, 75);		
+		gameloop.addSprite(shooter);
+	}
+
+	private void renderBackground() {
 		viewMatrix.setToOrtho2D(0, 0, 480, 800);
 		spriteBatch.setProjectionMatrix(viewMatrix);
 		spriteBatch.setTransformMatrix(transformMatrix);
@@ -57,24 +77,34 @@ public class GameLoopRenderer {
 		int i = 0;
 		for (int row = 0; row < 4; row++) {
 			for (int col = 0; col < 2; col++) {
-				spriteBatch.draw(background.getRegions().get(i).getTexture(), 0+col*256, (800-256)-row*256, 256, 256, 0, 0, 256,256, false, false);
-					i++;
+				spriteBatch.draw(background.getRegions().get(i).getTexture(),
+						0 + col * 256, (800 - 256) - row * 256, 256, 256, 0, 0,
+						256, 256, false, false);
+				i++;
 			}
 		}
-//		spriteBatch.draw(background.getRegions().get(i), 0 - 272, 0 - 112, 1024, 1024, 0, 0, 1024,1024, false, false);	
+		spriteBatch.end();
+	}
+
+	private void renderShooter(AnimatedSprite animSprite) {
+		spriteBatch.begin();
+		spriteBatch.enableBlending();
+		spriteBatch.setBlendFunction(GL10.GL_ONE, GL10.GL_ONE_MINUS_SRC_ALPHA);
+		spriteBatch.draw(animSprite, animSprite.getX(), animSprite.getY());
+		spriteBatch.disableBlending();
 		spriteBatch.end();
 	}
 
 	public static void draw(String text, int x, int y, Color color) {
 		spriteBatch.begin();
 		spriteBatch.enableBlending();
-		font.setColor(color); //Vil nok unødigt sænke performance
+		font.setColor(color); // Vil nok unødigt sænke performance
 		font.draw(spriteBatch, text, x, y);
 		spriteBatch.disableBlending();
 		spriteBatch.end();
 	}
 
 	public void dispose() {
-		
+
 	}
 }
