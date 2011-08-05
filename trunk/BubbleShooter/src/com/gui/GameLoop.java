2968a280f715dab569aa73155c0f4d5b2ca47b4e
askebisgaard@gmail.com <askebisgaard@gmail.com@7185608a-03ba-cb5d-678c-909f53f12cc8>
2011-08-05 20:17:46 +0000

    Første skridt mod at efterligne metagun's håndteringer

--- trunk/BubbleShooter/src/com/gui/GameLoop.java
+++ trunk/BubbleShooter/src/com/gui/GameLoop.java
@@ -1,5 +1,7 @@
 package com.gui;
 
+import java.util.ArrayList;
+
 import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
@@ -16,6 +18,8 @@ public class GameLoop implements Frame {
 	@SuppressWarnings("unused")
 	private MainInputProcessor inputProcessor;
 	private AnimatedSprite shooter;
+	private AnimatedSprite activeBubble;
+	private ArrayList<AnimatedSprite> bubbles;
 	private int shooterRotation = 0;
 
 	public GameLoop(Application app) {
@@ -27,6 +31,7 @@ public class GameLoop implements Frame {
 		simulation = new Simulation(this);
 		renderer = new GameLoopRenderer(this);
 		inputProcessor = new MainInputProcessor(this);
+		bubbles = new ArrayList<AnimatedSprite>();
 	}
 
 	@Override
@@ -38,7 +43,7 @@ public class GameLoop implements Frame {
 	public void render(Application app) {
 		app.getGraphics().getGL10().glClear(GL10.GL_COLOR_BUFFER_BIT);
 		renderer.render(app, simulation);
-		GameLoopRenderer.draw("FPS: " + Gdx.graphics.getFramesPerSecond(), 150,
+		GameLoopRenderer.drawText("FPS: " + Gdx.graphics.getFramesPerSecond(), 150,
 				150, Color.RED);
 	}
 
@@ -61,26 +66,65 @@ public class GameLoop implements Frame {
 		this.shooter = shooter;
 	}
 
+	public void setActiveBubble(AnimatedSprite activeBubble) {
+		this.activeBubble = activeBubble;
+	}
+
 	@Override
 	public boolean touchUp(int x, int y, int pointer, int button) {
-		int cY = -y + 800; // y converted from input- to
-							// animatedSprite-coordinates
-		if (cY > shooter.getOriginY()) {
-			int newRotAngle = (int) -Math.toDegrees(Math.atan((x - (shooter
-					.getOriginX() + shooter.getX()))
-					/ (cY - (shooter.getOriginY() + shooter.getY()))));
-			if (newRotAngle > -60 && newRotAngle < 60) {
-				shooter.rotate(-shooterRotation + newRotAngle);
-				shooterRotation = newRotAngle;
-				
+		activeBubble.setActive(false);
+		if (!activeBubble.isActive()) {
+			int cY = -y + 800; // y converted from input- to
+								// animatedSprite-coordinates
+			if (cY > shooter.getOriginY()) {
+				int newRotAngle = (int) -Math.toDegrees(Math.atan((x - (shooter
+						.getOriginX() + shooter.getX()))
+						/ (cY - (shooter.getOriginY() + shooter.getY()))));
+				if (newRotAngle > -60 && newRotAngle < 60) {
+					shooter.rotate(-shooterRotation + newRotAngle);
+					shooterRotation = newRotAngle;
+					shootBubble(newRotAngle);
+				}
 			}
 		}
 		return true;
 	}
 	
-	public void shootBuble(int rotation){
-		
+	public void shootBubble(double angle){
+		int dist = 40;
+		double bC = Math.toDegrees(Math.sin(90));
+		double a = -( dist * Math.toDegrees(Math.sin(Math.toRadians(angle))) ) / bC;
+		double b = ( dist * Math.toDegrees(Math.sin(Math.toRadians(180-angle-90))) ) / bC;
+		int x = (int) (a + shooter.getOriginX()+shooter.getX());
+		int y = (int) (b + shooter.getOriginY()+shooter.getY());
+		System.out.println("a: "+a + " b: "+b);
+		System.out.println("x: "+x + " y: "+y);
+		activeBubble.setPosition(x-activeBubble.getOriginX(),
+				y-activeBubble.getOriginY());
+		activeBubble.setActive(true);
+		animateActiveBubble(angle);
 	}
+	
+	private void animateActiveBubble(double angle) {
+		while (activeBubble.isActive()) {
+			double speed = 1;
+			double bC = Math.toDegrees(Math.sin(90));
+			double a = -( speed * Math.toDegrees(Math.sin(Math.toRadians(angle))) ) / bC;
+			double b = ( speed * Math.toDegrees(Math.sin(Math.toRadians(180-angle-90))) ) / bC;
+			
+			boolean changedDirection = false;
+			while(!changedDirection) {
+				try {
+					Thread.sleep(20);
+					activeBubble.setPosition(activeBubble.getX()+(float)a, activeBubble.getY()+(float)b);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	
+	
 
 	@Override
 	public void addSprite(AnimatedSprite sprite) {
